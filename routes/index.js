ab40b76c3d5138ca9c96d9bd4ef3b893498a6a47
Salman Javed <msalmanjvd@gmail.com>
2021-08-09 16:37:51 +0500

    version 3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { version: "V2" });
+  res.render("index", { version: "V3" });
 });
 
 module.exports = router;
